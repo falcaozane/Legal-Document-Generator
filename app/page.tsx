09d14381f46514102d30b460864c096c089a3765
Falcao Zane Vijay <112317470+falcaozane@users.noreply.github.com>
2023-12-15 02:04:23 +0530

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { useChat } from "ai/react";
 import { Document, Paragraph, Packer } from 'docx';
 import { saveAs } from 'file-saver';
 import { blob } from "stream/consumers";
+import { ClipboardDocumentIcon, DocumentArrowDownIcon, DocumentTextIcon } from '@heroicons/react/24/outline'
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
@@ -65,27 +66,28 @@ export default function Chat() {
   
 
   return (
-    <div className="mx-auto w-full max-w-xl py-24 flex flex-col stretch">
+    <div className="mx-auto w-full max-w-4xl py-24 flex flex-col stretch">
       {messages.length > 0
   ? messages.map((m) => (
-    <div key={m.id} className="whitespace-pre-wrap ">
+    <div key={m.id} className="whitespace-pre-wrap mx-2">
       {m.role === "user" ? <p className="font-semibold text-normal text-[#1A63F1]">You</p> : <p className="text-[#277763] font-semibold">AI</p>}
       {m.role === "user" ? (
         <p className="px-4 py-3 text-sm bg-blue-50 rounded-lg mt-1 mb-4">{m.content}</p>
       ) : (
         <>
           <div contentEditable='true' className="rounded-xl focus:outline-none focus:ring-2 focus:ring-lime-900/10">
-            <p className="text-justify    mt-2 px-5 py-8 font-normal text-sm bg-gray-50 rounded-lg" id={m.id}>{m.content}</p>
+            <p className="text-justify mt-2 px-5 py-8 font-normal text-sm bg-gray-50 rounded-lg" id={m.id}>{m.content}</p>
           </div>
           <div className="text-xs cursor-pointer my-1  text-gray-500"> <span className="text-red-500">*</span> Tap to edit the content</div>
-          <div className="my-6">
+          <div className="my-6 px-2">
             <button contentEditable='false' onClick={() => {
               const element = document.getElementById(m.id);
               if (element) {
                 navigator.clipboard.writeText(element.innerText);
               }
-            }} className="bg-[#53BDE1]  text-white font-semibold text-sm  py-2 px-4 rounded-lg">
-              Copy
+            }} className="bg-[#53BDE1] hover:bg-sky-600 text-white font-semibold text-sm  py-2 px-4 rounded-lg ">
+              <p className="hidden sm:inline">Copy</p>
+              <ClipboardDocumentIcon className="text-white h-5 w-5 sm:hidden" />
             </button>
             <button className="bg-[#19BA92] hover:bg-gray-800 text-white mx-2 font-semibold text-sm py-2 px-4 rounded-lg" onClick={
                 () => {
@@ -95,7 +97,9 @@ export default function Chat() {
                   }
                 }
               }>
-              Download Pdf
+              <p className="hidden sm:inline">Download PDF</p>
+              <DocumentArrowDownIcon className="text-white h-5 w-5 sm:hidden" />
+              
             </button>
             <button className="bg-[#19BA92] hover:bg-gray-800 text-white font-semibold text-sm py-2 px-4 rounded-lg " onClick={
                 () => {
@@ -105,7 +109,8 @@ export default function Chat() {
                   }
                 }
               }>
-              Download Docs
+              <p className="hidden sm:inline">Download Docs</p>
+              <DocumentTextIcon className="text-white h-5 w-5 sm:hidden" />
             </button>
           </div>
         </>
@@ -113,18 +118,18 @@ export default function Chat() {
     </div>
   ))
   : null}
-
+      <div className="">
       <form onSubmit={handleSubmit} className="">
         <div className="">
-
           <input
-            className="fixed h-12 w-full rounded-xl py-3 px-4 text-sm max-w-xl bottom-0 border border-gray-300  mb-8 focus:outline-none focus:ring-4 focus:ring-blue-500/10 "
+            className="fixed h-12 w-full rounded-xl py-3 px-4 text-sm max-w-4xl bottom-0 border border-gray-300  mb-8 focus:outline-none focus:ring-4 focus:ring-blue-500/10 "
             value={input}
             placeholder="Say something..."
             onChange={handleInputChange}
             />
         </div>
       </form>
+      </div>
     </div>
   );
 }
